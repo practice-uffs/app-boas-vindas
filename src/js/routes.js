dcec0d0ba30a0577d68562c7a5113891ca9e1e97
Alisson <alisson.luan2000@gmail.com>
2021-11-03 10:36:21 -0300

    rm unused code

--- src/js/routes.js
+++ src/js/routes.js
@@ -2,19 +2,13 @@ import InitialPage from "../pages/initial.f7.html";
 import HomePage from "../pages/home.f7.html";
 import RightPanelPage from "../pages/right-panel.f7.html";
 
-import EnvPage from "../pages/env.f7.html";
 import ServicesPage from "../pages/services.f7.html";
 
-import NotificationsPage from "../pages/notifications.f7.html";
-import SettingsPage from "../pages/settings.f7.html";
 import AboutPage from "../pages/about.f7.html";
 import NotFoundPage from "../pages/404.f7.html";
 import ContentPage from "../pages/content-page.f7.html";
 
-
-import { storage } from "../js/storage.js";
 import IsEnabled from "./isenabled";
-import App from '../js/app.js';
 
 //routes from template-apps-f7
 
@@ -23,23 +17,13 @@ const homePageRoute = function () {
     path: "/",
     component: HomePage,
   };
-  
-
   let tabs = [];
 
-  if (IsEnabled.servicesPage)
-    tabs.push({
-      path: "/",
-      id: "services",
-      component: ServicesPage,
-    });
-
-  if (IsEnabled.envPage)
-    tabs.push({
-      path: "/env/",
-      id: "env",
-      component: EnvPage,
-    });
+  tabs.push({
+    path: "/",
+    id: "services",
+    component: ServicesPage,
+  });
 
   route.tabs = tabs;
   return route;
@@ -54,24 +38,6 @@ const rightPanelRoute = function () {
   };
 
   if (IsEnabled.rightPanel) return route;
-};
-
-const notificationsPageRoute = function () {
-  let route = {
-    path: "/notifications/",
-    component: NotificationsPage,
-  };
-
-  if (IsEnabled.notificationsPage) return route;
-};
-
-const settingsPageRoute = function () {
-  let route = {
-    path: "/settings/",
-    component: SettingsPage,
-  };
-
-  if (IsEnabled.settingsPage) return route;
 };
 
 const aboutPageRoute = function () {
@@ -110,8 +76,6 @@ const contentPageRoute = function () {
 var routes = [
   homePageRoute(),
   rightPanelRoute(),
-  notificationsPageRoute(),
-  settingsPageRoute(),
   aboutPageRoute(),
   
   //new routes
